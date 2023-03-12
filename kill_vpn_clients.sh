e198f96d1d9613a731dd85d0d03e4364311eca8c
Tyler Hall <84144784+thalla2@users.noreply.github.com>
2023-03-11 17:57:11 -0700

    Moved to dynamic variables

--- kill_vpn_clients.sh
+++ kill_vpn_clients.sh
@@ -2,19 +2,21 @@
 
 # Variables
 NOW=`date +%s`
-KILLTIME=10800 # Kill VPN clients after 3 hours of being connected
+PORT=7505
+KILLTIME=43200
+TMPFILE=/tmp/current-users.vpn
 
 # Generate a list of connected employees
-echo 'status' | nc localhost 7505 | grep ^CLIENT_LIST | sed 's/ //g' > /tmp/current-users.vpn
+echo 'status' | nc localhost $PORT | grep ^CLIENT_LIST | sed 's/ //g' > $TMPFILE
 
 date
 echo "--------------------------------"
 echo "List of current clients"
-cat /tmp/current-users.vpn
+cat $TMPFILE
 echo "--------------------------------"
 
 # Loop through them
-for i in `cat /tmp/current-users.vpn`
+for i in `cat $TMPFILE`
 do
   	USERID=`echo $i | awk -F ',' '{print $2}'`
         SESSIONID=`echo $i | awk -F ',' '{print $3}'`
@@ -23,12 +25,12 @@ do
         DIFFR=$((DIFF / 3600))
 
         if [ $DIFF -gt $KILLTIME ]; then
-                echo "KILLED: User $USERID (Session: $SESSIONID) has been connected $DIFFR hours, killing session."
-                echo "kill $SESSIONID" | nc localhost 7505
+                echo "KILLING: User $USERID (Session: $SESSIONID) has been connected $DIFFR hours, killing session."
+                echo "kill $SESSIONID" | nc localhost $PORT
 
         fi
 
 done
 
 # Cleanup
-rm -f /tmp/current-users.vpn
+rm -f $TMPFILE
